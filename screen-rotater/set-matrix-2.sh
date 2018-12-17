91b884687f9c4000a736d631abeb9163842effe5
Anatoly <east@thyloved.ru>
2018-12-17 21:09:09 +0300

    touchscreen rotater

--- screen-rotater/set-matrix-2.sh
+++ screen-rotater/set-matrix-2.sh
@@ -5,12 +5,11 @@ DEVICES=("Logitech Lenovo USB Optical Mouse" "HID 1aad:0001")
 
 for ix in ${!DEVICES[*]}
 do
-    echo $DEVICE
     # Search for device by signature
     devlist=$(xinput --list | grep "${DEVICES[$ix]}" | sed -n 's/.*id=\([0-9]\+\).*/\1/p')
     # Set transformation matrix property
     for dev in $devlist
     do
-        echo $dev
+        xinput set-prop $dev 'Coordinate Transformation Matrix' -1 0 1 0 1 0 0 0 1 
     done
 done
